43a1aae28c170ce04fc03f9d6ded6afb71578b58
Frost Red Lee <frostredlee@gmail.com>
2016-09-26 22:05:26 +0800

    add BinarySearch

---
+++ BinarySearch.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using std::vector;
+int rank(int key, const vector<int>& a);
+
+int rank(int key, const vector<int>& a) {
+    int lo = 0;
+    int hi = a.size() - 1;
+
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) {
+            hi = mid - 1;
+        } else if (key > a[mid]) {
+            lo = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
